1e200d5bac2b5002312784e2f0c3cb87382a3734
vinodcse99 <vinod.gogula6@gmail.com>
2020-03-10 22:13:54 +0530

    automation repo

---
+++ src/com/healthcare/MyHealth.java
@@ -0,0 +1,12 @@
+package com.healthcare;
+
+public class MyHealth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("your health is good");
+
+	}
+
+}
